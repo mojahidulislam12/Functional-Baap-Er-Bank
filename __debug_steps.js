dd54675a1ce980f438c15023255a5fe0b55258d3
Sagor <mojahidulislamsagor1234@gmail.com>
2024-08-12 01:06:08 -0400

    first commit

---
+++ __debug_steps.js
@@ -0,0 +1,13 @@
+/*
+Debugh Steps
+1.Go to live website.And try to reproduce the error.
+2.Check the console of the live website to see any error/exception.
+3.Read exception thoroughly.
+4.If the error is new to you : Copy the text of the error and search online.
+5.If a line number is showing,click on that line to see the code.
+6.Add breakpoint.RErun the code (Refresh the website).And see whether you are getting the expected values.
+
+
+
+
+*/
